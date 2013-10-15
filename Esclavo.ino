9a6ee22ba52f144acf3d3fefdfb25f3eb2762353
Pedro Valero <pedro.valero@estudiante.uam.es>
2013-10-15 17:11:41 +0200

    con 4 ultras

--- Esclavo.ino
+++ Esclavo.ino
@@ -3,29 +3,31 @@ Pequeño intento de programa que controlara el arduino micro
  Autor: Pedro Valero Mejia
  Funcion: Leer sensores y conocer en todo instante la posicion del enemigo
  Fecha creacion: 26/09/2013  20:29
-Prueba de git
-Prueba de git 23 (Kike)
  */
 
 //Librerias
 #include <Wire.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 #define FIRST 1
 //Declaro los pines que voy a utilizar. Son "macros".
 
-const int sensorNorth = A1;
-const int sensorEast = A2;
-const int sensorWest = A3;
-const int sensorSouth = A4;
+const int sensorNorth = A0;
+const int sensorEast = A1;
+const int sensorWest = A2;
+const int sensorSouth = A3;
 
-const int sonarA = A5;
-const int sonarB = A6;
-const int sonarC = A7;
+const int sonarA = A4;
+const int sonarB = A5;
+const int sonarC = A6;
+const int sonarD = A7;
 
 //Distancia de los sensores al origen, suponiendo que los colocamos en los ejes
-const double distA = 5;
-const double distB = 5;
-const double distC = 5;
+const double posSonarA = 5;
+const double posSonarB = 5;
+const double posSonarC = 5;
+const double posSonarD = 5;
 
 const int code = 4;//Codigo como esclavo
 const double sound=34.32;
@@ -36,95 +38,187 @@ struct POSITION{
   double y;
 };
 
-POSITION pos={0 , 0};
+POSITION pos={
+  0 , 0};
 
-const int sensorMin=800; //Valor a partir del cual consideramos que el sensor ha localizado al rival
+const int sensorMin=80; //Valor a partir del cual consideramos que el sensor ha localizado al rival
+const int sonarMax=300; //Valor a partir del cual consideramos que el sensor ha localizado al rival
+#define CM(sonar)  sonar/10
+//#define PITAGORAS(H, C)  sqrt(H*H-C*C) // NO se si esto valdria
 
+/*
+Metodo de comparacion de doubles.
+ Devuelve un un numero negativo, cero, o positivo
+ en caso de que arg1 sea menor, igual o mayor que arg2,
+ respectivamente
+ */
+int comp_double(const void *arg1, const void *arg2){
+  if (*(double*)arg1 < *(double*)arg2) return -1;
+  else if (*(double*)arg1 > *(double*)arg2) return 1;
+  else return 0;
+}
+
+
+/*
+Devuelve la mediana de un array de doubles.
+ Recibe un puntero a double, o nombre del array
+ y el numero de elementos del array.
+ */
+double median_double(double *array, size_t num){
+  qsort((void *) array, num, sizeof(double), comp_double);
+  return array[(num - 1) / 2];
+}
+
+/*
+Funcion que analiza los sensores de ultrasonidos
+ No devuelve nada y actualiza directamente los valores de la estructura pos
+ */
 void sonar(){
-}
+  float distanceA, distanceB, distanceC, distanceD;
 
-void firstSonar(){
-  unsigned long pulse;
-  float distanceA, distanceB, distanceC;
-  float tempY;
-  
-  //Primero vamos a leer los datos de los sensores
-  pinMode(sonarA, OUTPUT); //ponemos el pin como salida
-  digitalWrite(sonarA, HIGH); //lo activamos
-  delayMicroseconds(10); //esperamos~habra que ver el por que de este tiempo y si se puede bajar
-  digitalWrite(sonarA, LOW); //lo desactivamos
-  pinMode(sonarA, INPUT); //ponemos el pin como entrada
-  pulse=pulseIn(sonarA, HIGH); //Ya sabemos los microsegundos que ha tardado en llegar el sonido
-  
-  distanceA=((float(pulse/1000.0))*sound)/2;
-  
-  pinMode(sonarB, OUTPUT); //ponemos el pin como salida
-  digitalWrite(sonarB, HIGH); //lo activamos
-  delayMicroseconds(10); //esperamos~habra que ver el por que de este tiempo y si se puede bajar
-  digitalWrite(sonarB, LOW); //lo desactivamos
-  pinMode(sonarB, INPUT); //ponemos el pin como entrada
-  pulse=pulseIn(sonarB, HIGH); //Ya sabemos los microsegundos que ha tardado en llegar el sonido
-  
-  distanceB=((float(pulse/1000.0))*sound)/2;
-  
-  pinMode(sonarC, OUTPUT); //ponemos el pin como salida
-  digitalWrite(sonarC, HIGH); //lo activamos
-  delayMicroseconds(10); //esperamos~habra que ver el por que de este tiempo y si se puede bajar
-  digitalWrite(sonarC, LOW); //lo desactivamos
-  pinMode(sonarC, INPUT); //ponemos el pin como entrada
-  pulse=pulseIn(sonarC, HIGH); //Ya sabemos los microsegundos que ha tardado en llegar el sonido
-  
-  distanceC=((float(pulse/1000.0))*sound)/2;
-  
-  //Triangulacion con los sonares para detectar posicion y codificacion de la informacion
-  pos.x=(distB*distB-distanceB+distanceC)/(-2*distC+2*distB);
-  tempY=sqrt(distanceB-(pos.x-distB)*(pos.x-distB));
-  
-  if(pos.x*pos.x+(tempY-distA)*(tempY-distA)-distanceA>pos.x*pos.x+(-tempY-distA)*(-tempY-distA)-distanceA){
-    pos.y=-tempY;
-  }else{
-    pos.y=tempY;
+  if(pos.y>0){
+    distanceA=analogRead(sonarA);
+    if(distanceA<sonarMax){
+      pos.x=-sqrt(CM(distanceA)*CM(distanceA)-pos.y*pos.y);//Pitagoras
+    }
+    else{ 
+      distanceB=analogRead(sonarB);
+      if(distanceB<sonarMax){
+        pos.x=sqrt(CM(distanceB)*CM(distanceB)-pos.y*pos.y);//Pitagoras
+      }
+    }
   }
-  
+  else if(pos.y<0){
+    distanceD=analogRead(sonarD);
+    if(distanceD<sonarMax){
+      pos.x=-sqrt(CM(distanceD)*CM(distanceD)-pos.y*pos.y);//Pitagoras
+    }
+    else{ 
+      distanceC=analogRead(sonarC);
+      if(distanceC<sonarMax){
+        pos.x=sqrt(CM(distanceC)*CM(distanceC)-pos.y*pos.y);//Pitagoras
+      }
+    }
+  }
+  else{ // El enemigo esta en un lateral
+    if(pos.x>0){
+      distanceB=analogRead(sonarB);
+      if(distanceB<sonarMax){
+        pos.y=sqrt(CM(distanceB)*CM(distanceB)-pos.x*pos.x);//Pitagoras
+      }
+      else if(pos.x<0){
+        distanceC=analogRead(sonarC);
+        if(distanceC<sonarMax){
+          pos.y=-sqrt(CM(distanceC)*CM(distanceC)-pos.x*pos.x);//Pitagoras
+        }
+      }
+      else {//Esta en el otro lado
+        distanceD=analogRead(sonarD);
+        if(distanceD<sonarMax){
+          pos.y=-sqrt(CM(distanceD)*CM(distanceD)-pos.x*pos.x);//Pitagoras
+        }
+        else{
+          distanceA=analogRead(sonarA);
+          if(distanceA<sonarMax){
+            pos.y=sqrt(CM(distanceA)*CM(distanceA)-pos.x*pos.x);//Pitagoras
+          }
+        }
+      }
+    }
+  }
+  /*Mide como poco  11 y como maximo unos 100 y algo. De 11 no baja nunca y empieza a marcar 11 cuando esta  amenos de 14 cm. Es bastante direccional,
+   no cubre toda una esfera realmente sino un arco mas bien
+   Serial.println(analogRead(1));*/
+
 }
 
+/*
+Metodo de comunicacion con maestro
+ Envia al maestro la informacion sobre la posicion del rival relativa
+ */
 void receiveEvent(int howmany)
 {
   Wire.write((byte*)&pos, sizeof(pos)); //Informacion enviada al maestro
-  pos.x=0; //Lo ponemos a 0, si el maestro recibe (0,0) sera que el esclavo no ha tenido tiempo de hacer los calculos
-  pos.y=0;
+}
+
+/*
+Metodo de analisis de sensores infrarrojos.
+ Recorre todos los sensores infrarrojos hasta localizar al rival.
+ Actualiza el valor de pos.
+ Devuelve :
+ 0 encontro al rival
+ 1 si fallo en la busqueda
+ */
+int infrarrojo(){
+  double dist;
+  double distances[50];
+  double distance;
+  int i;
+
+  if((dist=analogRead(sensorNorth)) >= sensorMin){
+    for(i=0;i<50;i++){
+      distances[i]=dist;
+      dist=analogRead(sensorNorth);
+    }
+
+    pos.y=4800/(median_double(distances, 50)-20);
+    pos.x=0;
+    //Serial.println(pos.y);
+    //delay(100);
+  }
+  else if((dist=analogRead(sensorEast)) >= sensorMin){
+    for(i=0;i<50;i++){
+      distances[i]=dist;
+      dist=analogRead(sensorEast);
+    }
+    pos.x=4800/(median_double(distances, 50)-20);
+    pos.y=0;
+  }
+  else if((dist=analogRead(sensorWest)) >= sensorMin){
+    for(i=0;i<50;i++){
+      distances[i]=dist;
+      dist=analogRead(sensorWest);
+    }
+    pos.x=-4800/(median_double(distances, 50)-20);
+    pos.y=0;
+  }
+  else if((dist=analogRead(sensorSouth)) >= sensorMin){
+    for(i=0;i<50;i++){
+      distances[i]=dist;
+      dist=analogRead(sensorSouth);
+    }
+    pos.y=-4800/(median_double(distances, 50)-20);
+    pos.x=0;
+  }
 }
 
 void setup(){
   Wire.begin(code);//Me identifico como esclavo con codigo=code
   Wire.onReceive(receiveEvent); //Funcion a la que saltare al recibir una orden del maestro
   Serial.begin(9600);
+
+  delayMicroseconds(10);
   //Posibles cuentas para ajuste de sensores
 }
 
 void loop(){
-  double dist;
-  if((dist=analogRead(sensorNorth)) >= sensorMin){
-    pos.y=dist;
-    pos.x=0;
-  }
-  else if((dist=analogRead(sensorEast)) >= sensorMin){
-    pos.x=dist;
-    pos.y=0;
-  }
-  else if((dist=analogRead(sensorWest)) >= sensorMin){
-    pos.x=-dist;
-    pos.y=0;
-  }
-  else if((dist=analogRead(sensorSouth)) >= sensorMin){
-    pos.y=-dist;
-    pos.x=0;
-  }
-  else{
-    if(FIRST) firstSonar();
-    else sonar();
+
+  if(infrarrojo()!=0){
+    sonar();
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
